25cdb127ad8f8827d03fb183b4946ab99cd6a1df
sergeyBaranov12312 <baranov.c23@gmail.com>
2023-04-01 00:55:57 +0300

    *12

--- 3/server1.py
+++ 3/server1.py
@@ -1,6 +1,7 @@
+import math
 from flask import Flask,request,render_template
 from re import *
-import re
+
 app = Flask(__name__)
 @app.route('/')
 def index():
@@ -9,27 +10,37 @@ def index():
 def calc():
     if request.method == 'POST':
         text = request.form["qwerty"]
-        c = findall('\D+',text)
-        b = findall('\d+',text)
+        c = findall('[\\+,\\-,*,/,%,V]',text)
+        b = text.split(c[0])
         print(c)
         print(b)
-        print(c)
         if c[0] == "+":
-            s = int(b[0]) + int(b[1])
+            s = float(b[0]) + float(b[1])
             print(f"{b[0]} + {b[1]} = {s}")
             return f"{b[0]} + {b[1]} = {s}"
         if c[0] == "-":
-            s = int(b[0]) - int(b[1])
+            s = float(b[0]) - float(b[1])
             print(f"{b[0]} - {b[1]} = {s}")
             return f"{b[0]} - {b[1]} = {s}"
         if c[0] == "*":
-            s = int(b[0]) * int(b[1])
+            s = float(b[0]) * float(b[1])
             print(f"{b[0]} * {b[1]} = {s}")
             return f"{b[0]} * {b[1]} = {s}"
         if c[0] == "/":
-            s = int(b[0]) / int(b[1])
-            print(f"{b[0]} / {b[1]} = {s}")
-            return f"{b[0]} / {b[1]} = {s}"
+            try:
+                s = float(b[0]) / float(b[1])
+                print(f"{b[0]} / {b[1]} = {s}")
+                return f"{b[0]} / {b[1]} = {s}"
+            except ZeroDivisionError:
+                return "на 0 нельзя делить"
+        if c[0] == "%":
+            s = float(b[0]) / 100
+            print(f"{b[0]} %  = {s}")
+            return f"{b[0]} % = {s}"
+        if c[0] == "V":
+            s = math.sqrt(float(b[0]))
+            print(f"{b[0]} sqrt  = {s}")
+            return f"{b[0]} sqrt = {s}"
     return "Error"
 if __name__ == '__main__':
     app.run(port=8081,host='127.0.0.1')
